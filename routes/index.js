0de0cfb9c43c7ce9d1d7acc49ed2ce9dbfb172c9
Andriy Sikora <andriy.sikora@gmail.com>
2016-11-23 17:54:02 +0200

    add get req
    add GET request and data to server

--- routes/index.js
+++ routes/index.js
@@ -25,11 +25,57 @@ var carsSchema = new Schema({
 var Cars = mongoose.model('Cars', carsSchema);
 //get all cars from db
 router.get('/api/cars', function(req, res, next) {
-  Cars.find(function(err, cars){
-    if(err)
-      res.send(err);
-    res.json(cars);
-  });
+  console.log('I recieved GET request');
+
+  car1 = {
+    number:3467,
+    make:"Volvo",
+    owner:"Petro Schevcev",
+    phone:"+38050934533",
+    arriveDate: '2016-10-13',
+    departureDate: '2016-10-14'
+  }
+  car2 = {
+    number:6781,
+    make:"VW",
+    owner:"Sergiy Fedotov",
+    phone:"+380504259388",
+    arriveDate: '2016-10-16',
+    departureDate: '2016-10-21'
+  }
+  car3 = {
+    number:3359,
+    make:"Mazda",
+    owner:"Fedor Yuriiv",
+    phone:"+380664259324",
+    arriveDate: '2016-11-02',
+    departureDate: '2016-11-04'
+  }
+  car4 = {
+    number:8412,
+    make:"Renault",
+    owner:"Vasiliy Ivanov",
+    phone:"+380974258373",
+    arriveDate: '2016-11-05',
+    departureDate: '2016-11-07'
+  }
+  car5 = {
+    number:2349,
+    make:"Lada",
+    owner:"Andrii Slovian",
+    phone:"+380994338374",
+    arriveDate: '2016-11-06',
+    departureDate: '2016-11-07'
+  }
+
+  var carList = [car1, car2, car3, car4, car5];
+
+  res.json(carList);
+  // Cars.find(function(err, cars){
+  //   if(err)
+  //     res.send(err);
+  //   res.json(cars);
+  // });
 });
 // add new car to db
 router.post('/api/cars', function (req, res, next) {
